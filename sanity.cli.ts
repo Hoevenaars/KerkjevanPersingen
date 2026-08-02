f34faca7965a31fa735589607947f0d6256ac23e
Hoevenaars <nhoevenaars@gmail.com>
2026-08-02 18:02:42 +0200

    Add studioHost configuration to sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,5 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? '8le5jso9',
     dataset: process.env.SANITY_STUDIO_DATASET ?? 'production',
   },
+  studioHost: 'kerkje-van-persingen',
 });
